0a4375f7dd2412cf6203d6523b275cd9cfc2e17f
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-16 14:06:28 +0200

    Create Zadanie Samogłoska (sam).cpp

---
+++ Zadanie Samogłoska (sam).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char a;
+	cin >> a;
+	if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'y')
+		cout << "tak";
+	else cout << "nie";
+	return 0;
+}
