ab8b2ed8b41da4f4feb6ed5512b509e52430a413
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-16 14:11:17 +0200

    Create Zadanie Podzielne (pod).cpp

---
+++ Zadanie Podzielne (pod).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	long long int a, b, c;
+	cin >> a >> b >> c;
+	cout << (b / c) - (a - 1) / c;
+
+
+
+	return 0;
+}
