43456ed326070c145b6a152af3a6896ce388d8ae
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-16 14:05:03 +0200

    Create Zadanie Prostopadłościan (pro).cpp

---
+++ Zadanie Prostopadłościan (pro).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>  
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a * b * c << "\n";
+    cout << 2*(a*b+a*c+b*c) << "\n";
+    
+    return 0;
+}
