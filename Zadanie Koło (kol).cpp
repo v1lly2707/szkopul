66d8292318ca287645609a1acf27f7f4a9b98f69
v1lly2707 <146948997+v1lly2707@users.noreply.github.com>
2024-04-16 13:54:15 +0200

    Create Zadanie Koło (kol).cpp

---
+++ Zadanie Koło (kol).cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double r;
+	cin >> r;
+	cout << fixed << setprecision(3) << 3.14159265358979323846 * r * r << "\n";
+	cout << fixed << setprecision(3) << 2 * 3.14159265358979323846 * r << "\n";
+	return 0;
+}
